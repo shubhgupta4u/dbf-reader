0e91dedc4de095eecfd9fe6c33ca6e637008e10a
agpinheiro <agpinheiro.ti@gmail.com>
2023-11-21 12:21:18 -0300

    fix_tests_path

--- tests/dbf-reader.test.ts
+++ tests/dbf-reader.test.ts
@@ -3,9 +3,10 @@ import * as fs from "fs";
 import { Dbf } from '../lib/dbf';
 import { DataTable, Column } from '../lib/models/dbf-file';
 
+
 describe('Reading dbf file', function () {
   it('check sample2-polygon', function () {
-    var pathPrefix='E:/Drive D/Workspace/dbf-reader/dbf-reader';
+    var pathPrefix='E:/DEV/libs/dbf-reader';
 
     let buffer:Buffer=fs.readFileSync(pathPrefix+'/tests/sample-dbf/sample2-polygon.dbf')
     let datatable: DataTable = Dbf.read(buffer);
@@ -14,7 +15,7 @@ describe('Reading dbf file', function () {
     should().equal(datatable.rows.length,9);
   });
   it('check db4window', function () {
-    var pathPrefix='E:/Drive D/Workspace/dbf-reader/dbf-reader';
+    var pathPrefix='E:/DEV/libs/dbf-reader';
 
     let buffer:Buffer=fs.readFileSync(pathPrefix+'/tests/sample-dbf/db4.dbf')
     let datatable: DataTable = Dbf.read(buffer);
@@ -23,7 +24,7 @@ describe('Reading dbf file', function () {
     should().equal(datatable.rows.length,1);
   });
   it('check db4window', function () {
-    var pathPrefix='E:/Drive D/Workspace/dbf-reader/dbf-reader';
+    var pathPrefix='E:/DEV/libs/dbf-reader'
 
     let buffer:Buffer=fs.readFileSync(pathPrefix+'/tests/sample-dbf/db4window.dbf')
     let datatable: DataTable = Dbf.read(buffer);
@@ -32,7 +33,7 @@ describe('Reading dbf file', function () {
     should().equal(datatable.rows.length,3);
   });
   it('check VisualFoxPro', function () {
-    var pathPrefix='E:/Drive D/Workspace/dbf-reader/dbf-reader';
+    var pathPrefix='E:/DEV/libs/dbf-reader'
 
     let buffer:Buffer=fs.readFileSync(pathPrefix+'/tests/sample-dbf/VisualFoxPro.dbf')
     let datatable: DataTable = Dbf.read(buffer);
@@ -41,7 +42,7 @@ describe('Reading dbf file', function () {
     should().equal(datatable.rows.length,2);
   });
   it('check DateDbf', function () {
-    var pathPrefix='E:/Drive D/Workspace/dbf-reader/dbf-reader';
+    var pathPrefix='E:/DEV/libs/dbf-reader'
 
     let buffer:Buffer=fs.readFileSync(pathPrefix+'/tests/sample-dbf/DateDbf.dbf')
     let datatable: DataTable = Dbf.read(buffer);
